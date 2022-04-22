e9d308b062d09a453a275e8aa46d49cf0e49aed5
alieliacik <ali.eliacik@hotmail.com>
2022-04-21 23:09:57 -0400

    redux dev tools setup

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,14 +5,13 @@ import createSagaMiddleware from 'redux-saga'
 import rootReducer from './rootReducer'
 import rootSaga from './rootSaga'
 
-const sagaMiddleware = createSagaMiddleware()
-// const store = createStore(
-//   rootReducer,
-//   // composeEnhancers(applyMiddleware(ReduxThunk))
-//   composeEnhancers(applyMiddleware(sagaMiddlaware))
-// )
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+)
 
 sagaMiddleware.run(rootSaga)
 
