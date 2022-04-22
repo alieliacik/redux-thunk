9470883302f278983dfd21a6ce8b7a0768da71d5
alieliacik <ali.eliacik@hotmail.com>
2022-04-22 18:03:43 -0400

    env file modification

--- src/crud/cts.js
+++ src/crud/cts.js
@@ -4,8 +4,7 @@ const cts = async (method = 'get', endpoint, payload) => {
   try {
     const res = await axios({
       method,
-      // url: `${process.env.REACT_APP_API_BASE_URL}/${endpoint}`,
-      url: `https://fakestoreapi.com/${endpoint}`,
+      url: `${process.env.REACT_APP_API_BASE_URL}/${endpoint}`,
       data: payload,
     })
     return { data: res.data, error: null }
