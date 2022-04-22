f6e6b3089e4494199eb93bb5fc1cec94a4cb5292
alieliacik <ali.eliacik@hotmail.com>
2022-04-22 11:39:55 -0400

    imuutable integration

--- src/pages/ProductDetail/redux/reducer.js
+++ src/pages/ProductDetail/redux/reducer.js
@@ -1,29 +1,30 @@
 import initialState from './store'
 import actionTypes from './actionTypes'
+import Immutable from 'seamless-immutable'
 
 const productDetailsReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case actionTypes.PRODUCT_DETAILS_REQUEST:
-      return {
+      return Immutable({
         ...state,
-      }
+      })
     case actionTypes.PRODUCT_DETAILS_RECEIVE:
-      return {
+      return Immutable({
         ...state,
         selectedProduct: payload,
         loading: false,
         error: null,
-      }
+      })
     case actionTypes.PRODUCT_DETAILS_FAIL:
-      return {
+      return Immutable({
         ...state,
         error: payload,
-      }
+      })
     case actionTypes.SET_LOADING_ACTION:
-      return {
+      return Immutable({
         ...state,
         loading: true,
-      }
+      })
     default:
       return state
   }
--- src/pages/Products/redux/reducer.js
+++ src/pages/Products/redux/reducer.js
@@ -1,31 +1,31 @@
 import actionTypes from './actionTypes'
 import initialState from './store'
+import Immutable from 'seamless-immutable'
 
 const productsReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case actionTypes.SET_PRODUCTS_REQUEST:
-      return {
+      return Immutable({
         ...state,
-      }
+      })
     case actionTypes.SET_PRODUCTS_RECEIVE:
-      return {
+      return Immutable({
         ...state,
         products: payload,
         loading: false,
         error: null,
-      }
+      })
     case actionTypes.SET_PRODUCTS_FAIL:
-      return {
+      return Immutable({
         ...state,
         error: payload,
-      }
+      })
 
     case actionTypes.SET_LOADING_ACTION:
-      return {
+      return Immutable({
         ...state,
         loading: true,
-      }
-
+      })
     default:
       return state
   }
