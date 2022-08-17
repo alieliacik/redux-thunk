332b806ce64af4c40e28db3e5efaba3b1e89b7db
alieliacik <ali.eliacik@hotmail.com>
2022-08-16 21:04:55 -0400

    fix infinite loading spinner

--- src/crud/cts.js
+++ src/crud/cts.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const cts = async (method = 'get', endpoint, payload) => {
-  console.log(process.env.REACT_APP_API_BASE_URL)
+
   try {
     const res = await axios({
       method,
-      url: `${process.env.REACT_APP_API_BASE_URL}/${endpoint}`,
+      url: `${process.env.REACT_APP_API_BASE_URL}${endpoint}`,
       data: payload,
     })
     return { data: res.data, error: null }
